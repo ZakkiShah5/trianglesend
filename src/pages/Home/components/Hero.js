ddfb01bd50dc898ff72a28b8b7d24684a7be481c
Zakee ul Hassan <zakki5shah@gmail.com>
2024-12-16 19:30:24 -0300

    added some magic

--- src/pages/Home/components/Hero.js
+++ src/pages/Home/components/Hero.js
@@ -1,21 +1,63 @@
 import cover from '../../../assets/cover.avif'
+import ship from '../../../assets/slider/entrance.jpg'
+import guest from '../../../assets/slider/guests.jpg'
 import badge from '../../../assets/badge.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+import '../Home.css'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
 const Hero = () => {
+  const slides = [
+    {
+      image: cover,
+      title: '2024 Pirate Festival at Triangles End',
+      date: 'September 14-15 Rain or Shine!'
+    },
+    {
+      image: ship,
+      title: 'Exciting Adventures Await!',
+      date: 'Join the Crew Today!'
+    },
+    {
+      image: guest,
+      title: 'Exciting Adventures Await!',
+      date: 'Join the Crew Today!'
+    }
+  ]
   return (
-    <div className='font-pirate relative'>
-      <img
-        className='object-center bg-cover bg-center'
-        src={cover}
-        alt='cover'
-      />
-      <div className='absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+    <div className='relative font-pirate'>
+      <div className='absolute top-10 left-1/2 transform z-10 -translate-x-1/2 -translate-y-1/2'>
         <img src={badge} className='w-96' alt='badge' />
       </div>
-      <div className='absolute opacity-90 bottom-5 text-center left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-secondary p-2'>
-        <h1 className='text-5xl text-primary font-bold bg-secondary border-2 border-black'>2024 Pirate Festival at Triangles End</h1>
-        <p>September 14-15 Rain or Shine!</p>
-      </div>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 6000 }}
+        loop={true}
+        className='hero-swiper'
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index} className='h-full'>
+            <img
+              className='object-cover w-full h-[110vh]'
+              src={slide.image}
+              alt='cover'
+            />
+            <div className='absolute opacity-90 bottom-5 text-center left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-secondary p-2'>
+              <h1 className='text-5xl text-primary font-bold bg-secondary border-2 border-black'>
+                {slide.title}
+              </h1>
+              <p>{slide.date}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   )
 }
--- src/pages/Home/components/Overview.js
+++ src/pages/Home/components/Overview.js
@@ -1,10 +1,10 @@
-import ship from '../../../assets/ship.avif'
+import ship from '../../../assets/slider/ship.jpg'
 
 const Overview = () => {
   return (
     <div className='overview flex justify-around max-w-screen-xl mx-auto bg-primary text-white'>
       <div className='flex-1 relative p-10'>
-        <img className='h-[80vh]' src={ship} alt='adv' />
+        <img className='h-[80vh] object-cover w-full' src={ship} alt='adv' />
       </div>
       <div className='flex-1 p-10 font-pirate'>
         <h1 className='text-4xl text-secondary mb-2'>Overview</h1>
